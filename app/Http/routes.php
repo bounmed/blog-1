c4b9ebc5c62d4522d0681a7116025042238c518d
vagrant <vagrant@homestead>
2016-07-10 14:00:57 +0000

    Changed routing order

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,13 +14,13 @@
 Route::auth();
 
 Route::get('/', 'ArticleController@welcome');
-Route::get('/article/{article}', 'ArticleController@article');
+
 Route::post('/search', 'ArticleController@search');
 
-Route::post('/article/{article}/comment', 'CommentController@new');
-
 Route::get('/admin', 'AdminController@index');
+
+Route::post('/article/new/', 'AdminController@new');
 Route::get('/article/edit/{article}', 'AdminController@edit');
 Route::patch('/article/edit/{article}', 'AdminController@save');
-Route::post('/article/new/', 'AdminController@new');
-
+Route::get('/article/{article}', 'ArticleController@article');
+Route::post('/article/{article}/comment', 'CommentController@new');
