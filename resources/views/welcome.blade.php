c2f19a453a467a649cec2bd7d720148137400660
vagrant <vagrant@homestead>
2016-07-10 15:02:56 +0000

    forgit to commit welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                         Skrevet av <a href="index.php">{{ $article->author->name }}</a>
                         @if (Auth::user())
                             @if (\App\User::find(Auth::user()->id)->admin > 0)
-                                <a href="/article/edit/{{ $article->id }}"><span class="glyphicon glyphicon glyphicon-edit" style="color: #333;"></span></a>
+                                <a href="/edit/{{ $article->id }}"><span class="glyphicon glyphicon glyphicon-edit" style="color: #333;"></span></a>
                             @endif
                         @endif
                     </p>
